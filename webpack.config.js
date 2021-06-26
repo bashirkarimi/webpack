f12a9cf99b559c98f7de70e0530e775b18621a33
bashir.karimi <bashir.karimi@unic.com>
2021-06-26 14:16:19 +0200

    Add devServer and configure env mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/assets/js/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    usedExports: true,
-  },
+module.exports = mode => {
+  console.log('mode', mode)
+  return {
+    entry: './src/assets/js/main.js',
+    mode: mode.development ? 'development': 'production',
+    output: {
+      filename: mode.production ? '[name].[contenthash].js': '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      writeToDisk: true
+    },
+    optimization: {
+      usedExports: true,
+    },
+    plugins: [
+      new htmlWebpackPlugin()
+    ]
+  }
 };
